249704fdde72e5757518eb51b4b66b29d857932c
ajaykumartn <154232256+ajaykumartn@users.noreply.github.com>
2025-05-14 10:09:16 +0530

    Delete OpenFunds-Decentralized-Fundraising-App-main F copy_DL copy directory

--- OpenFunds-Decentralized-Fundraising-App-main F copy_DL copy/sentence.py
+++
@@ -1,4 +0,0 @@
-from sentence_transformers import SentenceTransformer
-# This will download the model if it doesn't exist
-model = SentenceTransformer('all-MiniLM-L6-v2')
-print("Model downloaded successfully!")
--- OpenFunds-Decentralized-Fundraising-App-main F copy_DL copy/update_embeddings_once.py
+++
@@ -1,5 +0,0 @@
-# script: update_embeddings_once.py
-from models.dl_similarity import update_embeddings
-
-update_embeddings()
-print("✅ Embeddings updated successfully.")
